1c9f73deef9bdcb21e6d6bd6aa5ca976bb39887c
Nikita Kharin <harin-1990@yandex.ru>
2024-10-09 21:58:33 +0400

    Итог

--- Module4/Program.cs
+++ Module4/Program.cs
@@ -4,40 +4,94 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+        //string[] favcolors = new string[3];
 
-        var color = Console.ReadLine();
+        //for (int i = 0; i < favcolors.Length; i++)
+        //{
+        //    Console.WriteLine("Введите любимый цвет номер {0}", i + 1);
+        //    favcolors[i] = Console.ReadLine();
+        //}
 
-        switch (color)
+        //foreach (var color in favcolors)
+        //{
+        //    switch (color)
+        //    {
+        //        case "red":
+        //            Console.BackgroundColor = ConsoleColor.Red;
+        //            Console.ForegroundColor = ConsoleColor.Black;
+
+        //            Console.WriteLine("Your color is red!");
+        //            break;
+
+        //        case "green":
+        //            Console.BackgroundColor = ConsoleColor.Green;
+        //            Console.ForegroundColor = ConsoleColor.Black;
+
+        //            Console.WriteLine("Your color is green!");
+        //            break;
+
+        //        case "cyan":
+        //            Console.BackgroundColor = ConsoleColor.Cyan;
+        //            Console.ForegroundColor = ConsoleColor.Black;
+
+        //            Console.WriteLine("Your color is cyan!");
+        //            break;
+        //        default:
+        //            Console.BackgroundColor = ConsoleColor.Yellow;
+        //            Console.ForegroundColor = ConsoleColor.Black;
+
+        //            Console.WriteLine("Your color is yellow!");
+        //            break;
+        //    }
+
+        //}
+
+
+        (string Name, string LastName, string Login, int LoginLength, bool HasPet, string[] favcolors, double Age) User;
+
+        for (int k = 0; k < 3; k++)
         {
-            case "red":
-                Console.BackgroundColor = ConsoleColor.Red;
-                Console.ForegroundColor = ConsoleColor.Black;
 
-                Console.WriteLine("Your color is red!");
-                break;
+            Console.WriteLine("Введите имя");
+            User.Name = Console.ReadLine();
 
-            case "green":
-                Console.BackgroundColor = ConsoleColor.Green;
-                Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("Введите фамилию");
 
-                Console.WriteLine("Your color is green!");
-                break;
+            User.LastName = Console.ReadLine();
 
+            Console.WriteLine("Введите логин");
 
-            case "cyan":
-                Console.BackgroundColor = ConsoleColor.Cyan;
-                Console.ForegroundColor = ConsoleColor.Black;
+            User.Login = Console.ReadLine();
 
-                Console.WriteLine("Your color is green!");
-                break;
+            User.LoginLength = User.Login.Length;
 
-            default:
-                Console.BackgroundColor = ConsoleColor.Yellow;
-                Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Есть ли у вас животные? Да или Нет");
 
-                Console.WriteLine("Your color is cyan!");
-                break;
+            var result = Console.ReadLine();
+
+            if (result == "Да")
+            {
+                User.HasPet = true;
+            }
+            else
+            {
+                User.HasPet = false;
+            }
+
+            Console.WriteLine("Введите возраст пользователя");
+
+            User.Age = double.Parse(Console.ReadLine());
+
+            User.favcolors = new string[3];
+            Console.WriteLine("Введите три любимых цвета пользователя");
+
+            for (int i = 0; i < User.favcolors.Length; i++)
+            {
+                User.favcolors[i] = Console.ReadLine();
+            }
         }
+
+
+
     }
 }
